44398a694d35fe247f734dbc889d6f9c74550e26
Shagu13 <147240025+Shagu13@users.noreply.github.com>
2023-12-13 01:51:11 +0400

    First VS code push

---
+++ Homework_1/Homework_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Shalva Adeishvili");
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("just to type the input");
+        }
+    }
+}
